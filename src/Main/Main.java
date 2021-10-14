33df5694ee01dc23791d13799ea5c836474e1e96
DragonZ <drag.popescu@yahoo.com>
2021-10-14 13:34:23 +0300

    Aufgabe 2 Finished

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {4,8,3,10,17};
         System.out.println(aufgabe_1(numbers));
+        System.out.println(aufgabe_2(numbers));
 
     }
 
     public static int aufgabe_1(int [] numbers){
-        //returns the maximul value of the array
+        //returns the maximal value of the array
         int max_val=numbers[0];
         for(int value:numbers){
             if(value>max_val)
@@ -17,4 +18,15 @@ public class Main {
         }
         return max_val;
     }
+
+    public static int aufgabe_2(int [] numbers){
+        //returns the minimal value of the array
+        int min_val=numbers[0];
+        for(int value:numbers){
+            if(value<min_val)
+                min_val=value;
+        }
+        return min_val;
+    }
+
 }
